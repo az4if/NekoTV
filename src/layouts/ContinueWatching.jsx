9901a8bb2f01c00b97d5cc061310f626503975f2
JsmAtom <jsmatom72@hotmail.com>
2025-08-26 23:57:33 +0600

    Update ContinueWatching.jsx

--- src/layouts/ContinueWatching.jsx
+++ src/layouts/ContinueWatching.jsx
@@ -95,6 +95,7 @@ const ContinueWatching = () => {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3">
         {continueList.map((item, index) => {
           const thumb =
+            item?.poster ||
             item?.thumbnail ||
             `https://via.placeholder.com/320x180?text=${encodeURIComponent(
               formatAnimeName(item?.animeName || item?.animeId)
